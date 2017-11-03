9fb8079134126ea50b7b954af830d6f58a8817eb
mattdyer <madmatt1220@gmail.com>
2017-11-02 21:08:04 -0600

    updated Unity IAP - Working on Google drive storage

--- Assets/Scripts/Storage.cs
+++ Assets/Scripts/Storage.cs
@@ -1,4 +1,4 @@
-/*using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
 using Google.Apis.Services;
@@ -10,65 +10,68 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using UnityEngine;
 
-namespace DriveQuickstart
-{
-    class Program
+public class Storage : MonoBehaviour {
+    // If modifying these scopes, delete your previously saved credentials
+    // at ~/.credentials/drive-dotnet-quickstart.json
+    static string[] Scopes = {  DriveService.Scope.DriveReadonly };
+    static string ApplicationName = "Wakeboarding Mayhem";
+
+    //client id
+    //656522469414-4bvr6p7mhu5p627t0bijjvugrfv8a21a.apps.googleusercontent.com
+
+    //client secret
+    //Q3W6eF1oge_6jqR1nBVAi9FK
+
+    void Start()
     {
-        // If modifying these scopes, delete your previously saved credentials
-        // at ~/.credentials/drive-dotnet-quickstart.json
-        static string[] Scopes = {  DriveService.Scope.DriveReadonly };
-        static string ApplicationName = "Drive API .NET Quickstart";
+        UserCredential credential;
 
-        static void Main(string[] args)
+        using (var stream =
+            new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
         {
-            UserCredential credential;
+            string credPath = System.Environment.GetFolderPath(
+                System.Environment.SpecialFolder.Personal);
+            credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
 
-            using (var stream =
-                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
-            {
-                string credPath = System.Environment.GetFolderPath(
-                    System.Environment.SpecialFolder.Personal);
-                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
-
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
-
-            // Create Drive API service.
-            var service = new DriveService(new BaseClientService.Initializer()
-                {
-                    HttpClientInitializer = credential,
-                    ApplicationName = ApplicationName,
-                });
-
-            // Define parameters of request.
-            FilesResource.ListRequest listRequest = service.Files.List();
-            listRequest.PageSize = 10;
-            listRequest.Fields = "nextPageToken, files(id, name)";
-
-            // List files.
-            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
-                .Files;
-            Console.WriteLine("Files:");
-            if (files != null && files.Count > 0)
-            {
-                foreach (var file in files)
-                {
-                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No files found.");
-            }
-            Console.Read();
-
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.Load(stream).Secrets,
+                Scopes,
+                "user",
+                CancellationToken.None,
+                new FileDataStore(credPath, true)).Result;
+            Console.WriteLine("Credential file saved to: " + credPath);
         }
+
+        // Create Drive API service.
+        var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+        // Define parameters of request.
+        FilesResource.ListRequest listRequest = service.Files.List();
+        listRequest.PageSize = 10;
+        listRequest.Fields = "nextPageToken, files(id, name)";
+
+        // List files.
+        IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+            .Files;
+        Console.WriteLine("Files:");
+        if (files != null && files.Count > 0)
+        {
+            foreach (var file in files)
+            {
+                Console.WriteLine("{0} ({1})", file.Name, file.Id);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No files found.");
+        }
+        Console.Read();
+
     }
-}*/
+}
